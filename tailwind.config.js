88a21ea3b8c2b04b8416fda522a1940224d9cb3e
Camililita <algobvio@gmail.com>
2025-05-04 19:49:30 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        typewriter: ['RoughTypewriter', 'monospace'],
+        lora: ['Lora', 'serif'],
+        belgian: ['SouthBelgian', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
 
