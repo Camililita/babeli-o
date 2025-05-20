13513a681a3b9c39fed5973472b4938ea0c03488
Camililita <algobvio@gmail.com>
2025-05-20 18:41:16 -0300

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -5,7 +5,7 @@ export default function Post({ user, location, title, poem, avatar }) {
   return (
     <div className="mb-10">
       <div className="flex items-center gap-2 mb-1">
-        <Image src={avatar} alt={user} width={24} height={24} className="rounded-full" />
+        <Image src={avatar} alt={user} width={24} height={24} className="rounded-full object-cover aspect-square" />
         <div>
           <p className="text-sm font-bold">{user}</p>
           <p className="text-xs text-[#1C2B24]/50">{location}</p>
