7a3e4f35835d1c8350eea4c1c838e547e8c8dacc
Camililita <algobvio@gmail.com>
2025-05-20 18:46:32 -0300

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -13,9 +13,9 @@ export default function Post({ user, location, title, poem, avatar }) {
       </div>
       <p className="font-bold mt-2 mb-2 uppercase tracking-wide">{title}</p>
       <p className="whitespace-pre-line leading-relaxed mb-4">{poem}</p>
-      <div className="flex gap-4">
-        <Image src="/icons/guardado.svg" alt="Guardar" width={20} height={20} />
-        <Image src="/icons/avioncito.svg" alt="Enviar" width={20} height={20} />
+      <div className="flex justify-end gap-4 mb-2">
+        <Image src="/icons/guardado.svg" alt="Guardar" width={28} height={28} />
+        <Image src="/icons/avioncito.svg" alt="Enviar" width={28} height={28} />
       </div>
       <hr className="border-t-2 border-dashed border-[#1C2B24]/40 my-6" />
     </div>
