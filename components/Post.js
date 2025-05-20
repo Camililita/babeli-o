82d8d603a4a4416ec4abe2d63e2a280f0b9b1ad3
Camililita <algobvio@gmail.com>
2025-05-20 19:23:38 -0300

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -11,14 +11,14 @@ export default function Post({ user, location, title, poem, avatar }) {
           <p className="text-xs text-[#1C2B24]/50">{location}</p>
         </div>
       </div>
-      <div className="ml-[38px]">
-        <p className="font-bold mt-2 mb-2 uppercase tracking-wide text-[13px]">{title}</p>
+      <div className="ml-[46px]">
+        <p className="font-extrabold mt-2 mb-2 uppercase tracking-wide text-[13px]">{title}</p>
         <p className="whitespace-pre-line leading-relaxed mb-4 text-[15px]">{poem}</p>
         <div className="flex justify-end gap-4 mb-2">
           <Image src="/icons/guardado.svg" alt="Guardar" width={28} height={28} />
           <Image src="/icons/avioncito.svg" alt="Enviar" width={28} height={28} />
         </div>
-        <hr className="border-t border-dashed border-[#1C2B24]/40 my-10" style={{ borderSpacing: '10px 0' }} />
+        <hr className="border-t border-dotted border-[#1C2B24]/40 my-10" style={{ borderWidth: '2px', borderSpacing: '12px' }} />
       </div>
     </div>
   );
