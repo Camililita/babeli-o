4293918957109063167128416291ea5d8d252a03
Camililita <algobvio@gmail.com>
2025-05-25 16:13:39 -0300

    Update feed.js

--- pages/feed.js
+++ pages/feed.js
@@ -10,35 +10,68 @@ export default function Feed() {
       location: 'González Catán, Argentina',
       avatar: '/profile/lalora.png',
       title: 'EL VECINO HABLA CON LOS PAJARITOS',
-      poem: `qué lenta\nes la danza\nde las nubes\n\nque deleitan\na los pájaros\n\nque miran\ndesde los cables\nde la ciudad.\n\nLas miran y comentan\nque los vientos allá arriba\ncorren rápido\n\ny que sus antepasados\nsabían volar\nsin nada a lo que aferrarse.`
+      poem: `qué lenta
+es la danza
+de las nubes
+
+que deleitan
+a los pájaros
+
+que miran
+desde los cables
+de la ciudad.
+
+Las miran y comentan
+que los vientos allá arriba
+corren rápido
+
+y que sus antepasados
+sabían volar
+sin nada a lo que aferrarse.`
     },
     {
       user: '@delfinasanda',
       location: 'Caballo Chiquito, Argentina',
       avatar: '/profile/delfinasanda.png',
       title: 'EL VECINO HABLA CON LOS PAJARITOS',
-      poem: `qué lenta\nes la danza\nde las nubes`
+      poem: `qué lenta
+es la danza
+de las nubes`
     },
     {
       user: '@elcaminante',
       location: 'Valle Azul, Argentina',
       avatar: '/profile/delfinasanda.png',
       title: 'TORMENTA EN CALMA',
-      poem: `la calma\nes apenas\nuna pausa\nen la boca del viento\n\nque recuerda\nque todo\nvuelve a girar`
+      poem: `la calma
+es apenas
+una pausa
+en la boca del viento
+
+que recuerda
+que todo
+vuelve a girar`
     },
     {
       user: '@margarita.m',
       location: 'Santa Lucía, Uruguay',
       avatar: '/profile/delfinasanda.png',
       title: 'EL NOMBRE DEL RÍO',
-      poem: `el río\nno tiene nombre\nhasta que alguien\nse detiene\na escucharlo`
+      poem: `el río
+no tiene nombre
+hasta que alguien
+se detiene
+a escucharlo`
     },
     {
       user: '@zorrito',
       location: 'Tilcara, Argentina',
       avatar: '/profile/delfinasanda.png',
       title: 'LADRIDO DE SOMBRA',
-      poem: `ladró\nla sombra\ny el eco\nfue a esconderse`
+      poem: `ladró
+la sombra
+y el eco
+fue a esconderse`
     }
   ];
 
@@ -71,9 +104,9 @@ export default function Feed() {
         </main>
 
         {/* Floating Write Button */}
-        <div className="fixed bottom-24 right-5 z-50 w-[56px] h-[56px] bg-black rounded-full shadow-xl flex items-center justify-center">
-  <Image src="/icons/lapiz.svg" alt="Escribir" width={36} height={36} />
-</div>
+        <div className="fixed bottom-24 right-5 z-50 w-[48px] h-[48px] bg-black rounded-full shadow-xl flex items-center justify-center">
+          <Image src="/icons/lapiz.svg" alt="Escribir" width={36} height={36} className="object-contain" />
+        </div>
 
         {/* Navbar */}
         <nav className="fixed bottom-0 left-0 right-0 bg-[#F9F9F7] border-t border-[#1C2B24]/20 flex justify-around items-center px-4 py-2 text-xs">
